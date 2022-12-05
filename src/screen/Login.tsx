e0c5e30198ebb7dea697f7005655ed697c462965
Ilham Muhammad <ilham.muhammad007008@gmail.com>
2022-12-05 14:41:35 +0700

    first-commit

---
+++ src/screen/Login.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+console.log("apapapap");
+
+const Login = () => {
+  return (
+    <View>
+      <Text>INI Login</Text>
+    </View>
+  );
+};
+
+export default Login;
